31b7e8444d9241baf3128e0d7e800f5f27199c7c
O3H <owen77stubbs@gmail.com>
2024-04-08 13:28:06 +0100

    dont need to call API for ValidateIcon

--- experimental/validation.go
+++ experimental/validation.go
@@ -1,7 +1,10 @@
 package experimental
 
 import (
-	"github.com/the-egg-corp/thundergo/util"
+	"bytes"
+	"errors"
+	"image"
+	_ "image/png"
 )
 
 type ValidatorResponse struct {
@@ -20,11 +23,29 @@ func ValidateManifest() (ValidatorResponse, error) {
 	return ValidatorResponse{}, nil
 }
 
-func ValidateIcon(data Base64String) (ValidatorResponse, error) {
-	body := IconValidatorParams{
-		IconData: data.String(),
+// func ValidateIcon(data Base64String) (ValidatorResponse, error) {
+// 	body := IconValidatorParams{
+// 		IconData: data.String(),
+// 	}
+
+// 	endpoint := "api/experimental/submission/validate/icon"
+// 	return util.JsonPostRequest[ValidatorResponse](endpoint, body)
+// }
+
+func ValidateIcon(data []byte) (bool, error) {
+	// Decode the image
+	img, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		return false, err
 	}
 
-	endpoint := "api/experimental/submission/validate/icon"
-	return util.JsonPostRequest[ValidatorResponse](endpoint, body)
+	bounds := img.Bounds()
+	width, height := bounds.Dx(), bounds.Dy()
+
+	// Verify dimensions
+	if width == 256 && height == 256 {
+		return true, nil
+	} else {
+		return false, errors.New("image dimensions did not match: 256x256")
+	}
 }
--- tests/validation_test.go
+++ tests/validation_test.go
@@ -1,34 +1,24 @@
 package tests
 
-// import (
-// 	TSGO "github.com/the-egg-corp/thundergo/experimental"
-// 	"github.com/the-egg-corp/thundergo/util"
-// 	"os"
-// 	"testing"
-// )
+import (
+	TSGO "github.com/the-egg-corp/thundergo/experimental"
+	"github.com/the-egg-corp/thundergo/util"
+	"os"
+	"testing"
+)
 
-// // Seems to need Authentication
-// func Test_ValidateIcon(t *testing.T) {
-// 	var err error
-// 	icon, err := os.ReadFile("../test_icon.png")
+func TestValidateIcon(t *testing.T) {
+	var err error
+	icon, err := os.ReadFile("../test_icon.png")
 
-// 	if err != nil {
-// 		t.Fatalf(err.Error())
-// 	}
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 
-// 	str := TSGO.Base64String(icon)
-// 	//fmt.Println(str)
+	valid, err := TSGO.ValidateIcon(icon)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 
-// 	var res TSGO.ValidatorResponse
-// 	res, err = TSGO.ValidateIcon(str)
-
-// 	if err != nil {
-// 		t.Fatalf(err.Error())
-// 	}
-
-// 	if res.Success == false {
-// 		t.Fatalf("expected success")
-// 	}
-
-// 	util.PrettyPrint(res)
-// }
+	util.PrettyPrint(valid)
+}
