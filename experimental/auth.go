ce1d56b1e7a4ab7bfb2e6f6c2966375572ba8299
O3H <owen77stubbs@gmail.com>
2025-05-17 11:30:52 +0100

    update deps

--- experimental/auth.go
+++ experimental/auth.go
@@ -1,8 +1,8 @@
 package experimental
 
 type AuthOptions struct {
-	Code        string `json:"code"`
-	RedirectURI string `json:"redirect_uri"`
+	Code        string  `json:"code"`
+	RedirectURI *string `json:"redirect_uri"`
 }
 
 type AuthResponse struct {
@@ -11,7 +11,27 @@ type AuthResponse struct {
 	SessionID string  `json:"session_id"`
 }
 
-// TODO: Implement this. Edit: forgor why I needed this
+// // Mimicks the 'Login with GitHub' button on the Thunderstore website.
+// //
+// // If RedirectURI is ni or unspecified, it will default to "127.0.0.1".
 // func LoginWithGithub(auth AuthOptions) (AuthResponse, error) {
+// 	return SendAuthComplete(auth, "github")
+// }
+
+// // Mimicks the 'Login with GitHub' button on the Thunderstore website.
+// //
+// // If RedirectURI is nil or unspecified, it will default to "127.0.0.1".
+// func LoginWithDiscord(auth AuthOptions) (AuthResponse, error) {
+// 	return SendAuthComplete(auth, "discord")
+// }
+
+// TODO: Implement this.
+// func SendAuthComplete(auth AuthOptions, provider string) (AuthResponse, error) {
+// 	// send code and redirect uri to thunderstore
+
+// 	if auth.RedirectURI == nil {
+// 		*auth.RedirectURI = "127.0.0.1"
+// 	}
+
 // 	return AuthResponse{}, nil
 // }
--- go.mod
+++ go.mod
@@ -1,20 +1,20 @@
 module github.com/the-egg-corp/thundergo
 
-replace github.com/the-egg-corp/thundergo => ./
+go 1.23.0
+toolchain go1.24.2
 
-go 1.22.1
+replace github.com/the-egg-corp/thundergo => ./
 
 require (
 	github.com/dustin/go-humanize v1.0.1
-	github.com/go-resty/resty/v2 v2.16.2
+	github.com/go-resty/resty/v2 v2.16.5
 	github.com/hashicorp/go-version v1.7.0
-	github.com/samber/lo v1.39.0
-	github.com/sanity-io/litter v1.5.5
+	github.com/samber/lo v1.50.0
+	github.com/sanity-io/litter v1.5.8
 )
 
-require golang.org/x/sync v0.10.0
-
 require (
-	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
-	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0
+	golang.org/x/text v0.25.0 // indirect
 )
